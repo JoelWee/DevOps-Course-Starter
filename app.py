ccfe792c7a25dfba9601aee3ac0a3541413403df
JoelWee <Joel.Wee@softwire.com>
2020-05-27 11:32:27 +0100

    Display list, allow add and complete

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
 from flask import Flask, render_template, request, redirect, url_for
-import session_items as session
+
+from session_items import get_items, add_item, get_item, save_item
 
 app = Flask(__name__)
 app.config.from_object('flask_config.Config')
 
-@app.route('/')
+
+@app.route('/', methods=["GET"])
 def index():
-    return 'Hello World!'
+    return render_template('index.html', items=get_items())
+
+
+@app.route('/', methods=["POST"])
+def add():
+    title = request.form.get('title')
+    add_item(title=title)
+    return redirect(url_for("index"))
+
+
+@app.route('/items/<int:item_id>', methods=["POST"])  # Should be patch, but can't do without a forms library
+def complete_item(item_id):
+    item = get_item(id=item_id)
+    item["status"] = "Complete"
+    save_item(item)
+    return redirect(url_for("index"))
+
 
 if __name__ == '__main__':
     app.run()
