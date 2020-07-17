81de56b6ae7f783de81b310308a6558d303658a7
Joel Wee <joel.wee@outlook.com>
2020-07-17 14:52:46 +0800

    Module 2: Refactor

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 
-from session_items import get_items, add_item, move_to_done
+from trello_client import Trello
 
 app = Flask(__name__)
 app.config.from_object("flask_config.Config")
@@ -8,13 +8,13 @@ app.config.from_object("flask_config.Config")
 
 @app.route("/", methods=["GET"])
 def index():
-    return render_template("index.html", items=get_items())
+    return render_template("index.html", items=Trello().get_items())
 
 
 @app.route("/", methods=["POST"])
 def add():
     title = request.form.get("title")
-    add_item(title=title)
+    Trello().add_item(title=title)
     return redirect(url_for("index"))
 
 
@@ -22,7 +22,7 @@ def add():
     "/items/<string:item_id>", methods=["POST"]
 )  # Should be patch, but can't do without a forms library
 def complete_item(item_id):
-    move_to_done(item_id)
+    Trello().move_to_done(item_id)
     return redirect(url_for("index"))
 
 
